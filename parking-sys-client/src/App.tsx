5706948cc1d6d2886f52df4145d9c30b7ba18dd6
Hassan Yusuf <mhyyusuf3@gmail.com>
2024-02-14 02:53:15 +0530

    Initial setup + play around with CSS

--- parking-sys-client/src/App.tsx
+++ parking-sys-client/src/App.tsx
@@ -1,23 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="bg-gradient-to-r from-cyan-500 to-blue-5000 h-full">
+      <header className="">
+        <h1 className="text-[200px] text-right">Welcome.</h1>
       </header>
     </div>
   );
