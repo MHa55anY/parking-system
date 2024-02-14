b79ebb026d490ed2b2a33c162a32d3768210871b
Hassan Yusuf <mhyyusuf3@gmail.com>
2024-02-15 02:19:18 +0530

    Login modal design complete

--- parking-sys-client/src/components/inputs/FormInput.tsx
+++ parking-sys-client/src/components/inputs/FormInput.tsx
@@ -1,12 +1,31 @@
+import { FC } from "react";
+import { useFormContext } from "react-hook-form";
+
 interface FormInputProps {
     //write props
+    name: string;
+    type: 'email' | "password" | "text" | "number";
+    label: string;
+    placeholder?: string;
 }
 
-const FormInput = () => {
+const FormInput: FC<FormInputProps> = ({
+  name,
+  type,
+  label,
+  placeholder
+}) => {
+  const { register } = useFormContext();
   return (
-    <div className="w-full flex items-center">
-        <label className="w-[30%] text-[2rem]">User</label>
-        <input type="text" placeholder="Write any silly username" className="w-[70%] rounded-md h-[50px]"/>
+    <div className="w-full flex items-center text-amber-500">
+        {/* <label className="w-[30%] text-[1.5rem]">{label}</label> */}
+        <input 
+          {...register(name)} 
+          type={type} 
+          placeholder={placeholder} 
+          className="w-full text-white rounded-full h-[50px] p-2 focus:placeholder:opacity-0 bg-gradient-to-r from-orange-500 to-orange-400
+                   placeholder:text-white pl-8"
+        />
     </div>
   )
 }
--- parking-sys-client/src/components/landingPage/LoginCard.tsx
+++ parking-sys-client/src/components/landingPage/LoginCard.tsx
@@ -1,16 +1,39 @@
+import { FieldValues, FormProvider, useForm } from "react-hook-form"
 import FormInput from "../inputs/FormInput"
+import { useState } from "react";
 
 const LoginCard = () => {
+  const [login, setLogin] = useState(true);
+  const methods = useForm<{email: string, password: string}>();
+  const { handleSubmit } = methods;
+  const onSubmit = (data: FieldValues) => console.log(data);
   return (
-    <div className="w-[500px] h-[500px] fixed p-5 bg-slate-300 right-[10%] top-[20%] rounded-md opacity-80 shadow-sm">
-        <header className="text-2xl font-bold text-center mb-[100px]">
-            Login or Sign Up
-        </header>
-        <div className="flex flex-col gap-2">
-            <FormInput />
-            <FormInput />
-        </div>
-    </div>
+    <FormProvider {...methods}>
+      <div 
+        className="w-[500px] h-[500px] fixed p-5 bg-white right-[10%] top-[20%] rounded-md opacity-80 shadow-sm" >
+          <header className="text-2xl text-orange-500 font-bold text-center mb-[100px]">
+            {login ? 'USER LOGIN' : 'CREATE ACCOUNT'}  
+          </header>
+          <div className="flex flex-col gap-8">
+              <FormInput name="email" type="email" label="Email" placeholder="Username" />
+              <FormInput name="password" type="password" label="Password" placeholder="Password" />
+          </div>
+          <div className="flex justify-center mt-10 text-lg">
+            <button 
+              type="submit" 
+              onClick={handleSubmit(onSubmit)}
+              className="bg-orange-600 hover:opacity-80 p-4 rounded-lg w-[200px] text-white"
+            >
+              {login ? 'Login' : 'Sign In'}
+            </button>
+          </div>
+          <footer className="flex flex-row justify-between mt-10">
+            <label className="text-orange-600 cursor-pointer" onClick={() => setLogin(!login)}>
+              {login ? 'Create new account!' : 'Already have an account? Login!'}
+            </label>
+          </footer>
+      </div>
+    </FormProvider>
   )
 }
 
