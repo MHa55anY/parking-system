ea4b1d3dd86ab4bb941805635843da55012b8589
Hassan Yusuf <mhyyusuf3@gmail.com>
2024-02-27 00:12:19 +0530

    Adding endpoint to access occupied slots

--- parking-sys-server/src/routes/parking.js
+++ parking-sys-server/src/routes/parking.js
@@ -1,22 +1,33 @@
 import express from 'express';
 import pool from '../models/common.js'
 import {authenticateToken} from '../middleware/authMiddleWare.js'
+import ParkingStates from '../types/ParkingStatesEnum.js';
 
 const parkingRoutes = express.Router();
 
-//expect -> [{code: 'dwdfd', coordinate: '0,0', userId: loggedInUser}]
-parkingRoutes.post('/add-parking-slots', authenticateToken, async (req, res) => {
+//expect -> {code: 'dwdfd', coordinate: '0,0', userId: loggedInUser}
+parkingRoutes.post('/occupy-parking-slot', authenticateToken, async (req, res) => {
     const {body} = req;
-    let query = 'INSERT INTO parking (code, coordinate, userId) VALUES'
-    const rowsToAdd = body.map((b) => {
-        return `('${b.code}', '${b.coordinate}', ${req.user.id})`
-    })
-    query += rowsToAdd + 'RETURNING *';
+    let query = 'INSERT INTO parking (code, coordinate, userId, status) VALUES'
+    const rowToAdd =`('${body.code}', '${body.coordinate}', ${req.user.id}, '${ParkingStates.OCCUPIED}')`
+    query += rowToAdd + 'RETURNING *';
 
     try {
         const client = await pool.connect();
         const {rows} = await client.query(query);
         return res.status(200).json({ success: true, message: 'Added slots!', rows})
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({message: error.message});
+    }
+});
+
+parkingRoutes.get('/fetch-occupied-slots', authenticateToken, async (req, res) => {
+    const query = `SELECT * FROM parking WHERE userId='${req.user.id}'`;
+    try {
+        const client = await pool.connect();
+        const {rows} = await client.query(query);
+        return res.status(200).json({ success: true, message: 'Loaded saved parking!', rows})
     } catch (error) {
         console.log(error)
         return res.status(500).json({message: error.message});
