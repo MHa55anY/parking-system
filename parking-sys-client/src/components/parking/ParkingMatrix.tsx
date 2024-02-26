cf51ee8d507a89f22b253d52b0706fda1688ec89
Hassan Yusuf <mhyyusuf3@gmail.com>
2024-02-27 00:14:21 +0530

    Load saved parking

--- parking-sys-client/src/components/parking/ParkingMatrix.tsx
+++ parking-sys-client/src/components/parking/ParkingMatrix.tsx
@@ -1,10 +1,11 @@
 import ParkingSlot from "./ParkingSlot"
 import ParkingModal from "./ParkingModal";
-import useParkVehicleModal from "../../hooks/useParkVehicleModal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import defaulltSlots from "./helper/defaultSlots";
 import IParkngSlot from "./types/IParkingSlot";
 import ParkingStates from "./types/ParkingStatesEnum";
+import { fetchOccupiedSlots } from "../../services/parkingSlot";
+import toast from "react-hot-toast";
 
 const ParkingMatrix = () => {
   const [parkingSlots, setParkingSlots] = useState<IParkngSlot[]>(defaulltSlots);
@@ -15,6 +16,24 @@ const ParkingMatrix = () => {
       return {...parking}
     })
   }
+
+  useEffect(() => {
+    const fetchOccupiedSlotsFn =  async () => {
+      try {
+        const { data }: {data: IParkngSlot[]} = await fetchOccupiedSlots();
+        setParkingSlots((prev) => {
+          data.forEach((occupiedSlot) => {
+            const index = prev.findIndex((p) => p.code === occupiedSlot.code);
+            if(index) prev[index] = occupiedSlot;
+          })
+          return [...prev];
+        })
+      } catch (error) {
+        toast.error("Could not load saved parking, please check if the server is running!")
+      }
+    };
+    fetchOccupiedSlotsFn();
+  }, [])
 
   return (
     <>
